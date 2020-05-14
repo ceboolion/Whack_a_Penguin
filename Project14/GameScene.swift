e03d940841191dea49e7b4e8856624182a2bd11a
Roman Cebula <ceboolion@gmail.com>
2020-05-14 13:32:43 +0200

    Initial setup of project

---
+++ Project14/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Roman Cebula on 14/05/2020.
+//  Copyright © 2020 Roman Cebula. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+        }
+        
+}
